55f66787ad539980cdb258e83c8ac7f4f1b9d53f
meteor <meteorshower471@gmail.com>
2026-03-24 15:10:42 +0530

    feature commit

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 def greet(name):
-    return f"Hello, {name}!"
+    return f"Hi, {name}!"   # changed line 💣
 
 def add(a, b):
     return a + b
 
 if __name__ == "__main__":
     print(greet("Mehul"))
-    print("Sum:", add(5, 3))
+    print("Total:", add(5, 3))   # changed text
